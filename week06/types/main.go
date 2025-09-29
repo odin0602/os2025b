a9b1b7008fc4ba377593e6875cd10aa9fda86bbf
odin0602 <hongjunkim69@gmail.com>
2025-09-29 15:33:23 +0900

    week06

--- week06/types/main.go
+++ week06/types/main.go
@@ -2,13 +2,30 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"strings"
+	"reflect"
 )
 
 func main() {
-	fmt.Println(math.Ceil(2.91))
-	fmt.Println(strings.Title("go developer~"))
-	fmt.Println("Kim\nInha\t\"\\")
-	fmt.Println('A', '가')
+	fmt.Println(reflect.TypeOf(2.31))
+	fmt.Println(reflect.TypeOf("go developer~"))
+	fmt.Println(reflect.TypeOf('a'))
+	fmt.Println(reflect.TypeOf(true))
+	fmt.Println(reflect.TypeOf(91))
+	var gpa float32 = 3.99
+	var name string = "kim inha"
+	var id int = 1000
+	var f64 float64
+	var i16 int16
+	var t bool
+	var s string
+	var i int
+
+	fmt.Println(name, reflect.TypeOf(name))
+	fmt.Println(id, reflect.TypeOf(id))
+	fmt.Println(gpa, reflect.TypeOf(gpa))
+	fmt.Println(f64, reflect.TypeOf(f64))
+	fmt.Println(i16, reflect.TypeOf(i16))
+	fmt.Println(t, reflect.TypeOf(t))
+	fmt.Println(s, reflect.TypeOf(s))
+	fmt.Println(i, reflect.TypeOf(i))
 }
