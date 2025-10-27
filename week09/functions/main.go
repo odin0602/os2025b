835898d09235ce787119f29979c7e7e75ba69ead
odin0602 <hongjunkim69@gmail.com>
2025-10-27 15:00:04 +0900

    week09/func

--- week09/functions/main.go
+++ week09/functions/main.go
@@ -2,16 +2,16 @@ package main
 
 import "fmt"
 
-func swap(first int, second int) {
-	temp := first
-	first = second
-	second = temp
-	fmt.Println(first, second)
+func swap(first *int, second *int) {
+	temp := *first
+	*first = *second
+	*second = temp
+	fmt.Println(*first, *second)
 }
 
 func main() {
 	a, b := 10, 20
 	fmt.Println(a, b)
-	swap(a, b)
+	swap(&a, &b)
 	fmt.Println(a, b)
 }
