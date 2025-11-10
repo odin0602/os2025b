328c94d0426b5226bb8459a8e300282146498d49
odin0602 <hongjunkim69@gmail.com>
2025-11-10 14:33:00 +0900

    week11

--- week11/cmd/ch06-01/main.go
+++ week11/cmd/ch06-01/main.go
@@ -3,17 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	//var subjects []string
-	//subjects = make([]string, 3)
 
-	//subjects := make([]string,3)
-
-	//subjects[0] = "Go"
-	//subjects[2] = "Python"
-
-	subjects := []string{"Go", "", "Python"} //slice 리터럴
+	subjects := []string{"Go", "Javascript", "Python", "Linux"} //slice 리터럴
+	subjectsSlice := subjects[1:3]                              //slicing
 
 	for _, subject := range subjects {
 		fmt.Println(subject)
+	}
+	fmt.Println("===========================================")
+	for i := 0; i < len(subjectsSlice); i++ {
+		fmt.Println(subjects)
 	}
 }
