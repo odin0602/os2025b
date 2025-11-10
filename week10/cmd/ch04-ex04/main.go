801b10540e2312e2c47f198e02ed0e9ce012a095
odin0602 <hongjunkim69@gmail.com>
2025-11-10 14:22:05 +0900

    week11

---
+++ week10/cmd/ch04-ex04/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"week10/pkg/greeting"
+	"week10/pkg/greeting/deutsch"
+	"week10/pkg/greeting/korean"
+)
+
+func main() {
+	greeting.Hello()
+	greeting.Hi()
+	deutsch.GutenTag()
+	korean.Annyeonghaseyo()
+	korean.Annyeong()
+}
---
+++ week10/pkg/greeting/korean/korean.go
@@ -0,0 +1,11 @@
+package korean
+
+import "fmt"
+
+func Annyeonghaseyo() {
+	fmt.Printf("안녕하세요.")
+}
+
+func Annyeong() {
+	fmt.Print(("안녕."))
+}
