036d06b3f18ae16da3a1f15604cff194781ba353
odin0602 <hongjunkim69@gmail.com>
2025-12-01 15:52:33 +0900

    week14-03

--- week11/cmd/ch06-03/main.go
+++ week11/cmd/ch06-03/main.go
@@ -2,9 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 )
 
+func mean(numbers ...float64) float64 {
+	var sum float64 = 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum / float64(len(numbers))
+}
+
 func main() {
-	fmt.Println(os.Args[1:])
+	arguments := os.Args[1:]
+	var numbers []float64
+	for _, argument := range arguments {
+		number, err := strconv.ParseFloat(argument, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		numbers = append(numbers, number)
+	}
+	fmt.Printf("평균 고기 소비량: %0.2f\n", mean(numbers...))
 }
